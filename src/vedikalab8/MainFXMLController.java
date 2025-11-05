80ca1c5cbde2d4b8afad08e3646b4c6c6d9efd30
Vedika894 <vjain894@yahoo.com>
2025-11-04 21:42:14 -0500

    added code to the fxml files

---
+++ src/vedikalab8/MainFXMLController.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package vedikalab8;
+
+
+ 
+import java.awt.Color;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
+public class MainFXMLController {
+
+    
+     // GUI Component References (fx:id must match Scene Builder) =====
+    @FXML private Pane drawingAreaPane;
+     
+     @FXML
+    private ToggleGroup colorToggleGroup;
+
+    @FXML
+    private ToggleGroup sizeToggleGroup;
+
+    @FXML private RadioButton blackRadioButton;
+    @FXML private RadioButton redRadioButton;
+    @FXML private RadioButton blueRadioButton;
+    @FXML private RadioButton greenRadioButton;
+
+    @FXML private RadioButton smallRadioButton;
+    @FXML private RadioButton mediumRadioButton;
+    @FXML private RadioButton largeRadioButton;
+
+    // ===== State Variables =====
+    private PenSize penSize = PenSize.MEDIUM; // default
+    private Color brushColor = Color.BLACK;  // default
+
+    
+    //event handlers 
+    @FXML
+    void clearButtonPressed(ActionEvent event) {
+        drawingAreaPane.getChildren().clear();
+    }
+    
+
+      @FXML
+    private void colorRadioButtonSelected() {
+        // Get selected RadioButton's color from its user data
+        brushColor = (Color) colorToggleGroup.getSelectedToggle().getUserData();
+    }
+
+    @FXML
+    private void sizeRadioButtonSelected() {
+        // Get selected RadioButton's pen size from its user data
+        penSize = (PenSize) sizeToggleGroup.getSelectedToggle().getUserData();
+    }
+
+
+    @FXML
+    void drawingAreaMouseDragged(MouseEvent event) {
+       // Create a circle where mouse is dragged
+        Circle newCircle = new Circle(event.getX(), event.getY(), penSize.getRadius(),brushColor );
+        drawingAreaPane.getChildren().add(newCircle);
+    }
+    
+
+    @FXML
+    private void undoButtonPressed() {
+        int size = drawingAreaPane.getChildren().size();
+        if (size > 0) {
+            drawingAreaPane.getChildren().remove(size - 1);
+        }
+    }
+    
+    private enum PenSize {
+        SMALL(2),
+        MEDIUM(4),
+        LARGE(6);
+
+        private final int radius;
+
+        PenSize(int radius) {
+            this.radius = radius;
+        }
+
+        public int getRadius() {
+            return radius;
+        }
+    }
+   
+
+    // ===== Initialize method called automatically after FXML loads =====
+    @FXML
+    private void initialize() {
+
+        // Attach UserData to color RadioButtons
+        blackRadioButton.setUserData(Color.BLACK);
+        redRadioButton.setUserData(Color.RED);
+        blueRadioButton.setUserData(Color.BLUE);
+        greenRadioButton.setUserData(Color.GREEN);
+
+        // Attach UserData to size RadioButtons
+        smallRadioButton.setUserData(PenSize.SMALL);
+        mediumRadioButton.setUserData(PenSize.MEDIUM);
+        largeRadioButton.setUserData(PenSize.LARGE);
+    }
+}
---
+++ src/vedikalab8/VedikaLab8.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vedikalab8;
+
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Vedika
+ */
+public class VedikaLab8 extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage){
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("MainFXML.fxml"));
+            stage.setTitle("FXML Welcome");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(VedikaLab8.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
